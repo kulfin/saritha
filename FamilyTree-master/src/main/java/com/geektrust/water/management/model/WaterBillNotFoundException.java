1be85929cbef33a47c9202210cc79f3a06593a0d
Vasudeva kulkarni <kulfin@rediffmail.com>
2022-02-14 11:25:03 +0530

    Add files via upload

---
+++ FamilyTree-master/src/main/java/com/geektrust/water/management/model/WaterBillNotFoundException.java
@@ -0,0 +1,31 @@
+package com.waterManagement.model;
+
+public class WaterBillNotFoundException extends RuntimeException {
+
+	public WaterBillNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public WaterBillNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WaterBillNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WaterBillNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WaterBillNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
